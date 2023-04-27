170f43666b141c76f0b39382c7c02f548b315e10
Stace-cmd <stacykerongo@gmail.com>
2023-04-27 08:19:53 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:  prints the alphabet in lowercase, and then in uppercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 97;
+while ( n <= 122)
+{
+putchar(n);
+n++;
+}
+n = 65;
+while ( n <= 90)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
+
