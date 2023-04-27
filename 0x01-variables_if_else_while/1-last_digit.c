5e98565ef1081c2548ce398f64d5b1a2a00fddce
Stace-cmd <stacykerongo@gmail.com>
2023-04-27 07:33:38 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,10 +6,9 @@
  * Description: 'get the last digit of a number'
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int n;
+int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 printf("Last digit of %d is ", n);
@@ -19,6 +18,5 @@ else if (n % 10 == 0)
 printf("%d and is 0\n", n % 10);
 else
 printf("%d and is less than 6 and not 0\n", n % 10);
-
     return (0);
 }
