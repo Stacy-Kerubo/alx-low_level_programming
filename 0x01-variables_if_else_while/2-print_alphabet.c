48d92d20ad4624046f6ceca670aeb173986cf88d
Stace-cmd <stacykerongo@gmail.com>
2023-04-27 07:59:47 -0700

    ask 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints alphabet lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main ()
+{
+int n = 97;
+while (n <=122)
+{
+	putchar (n);
+	n++;
+}
+putchar('\n');
+return (0);
+}
