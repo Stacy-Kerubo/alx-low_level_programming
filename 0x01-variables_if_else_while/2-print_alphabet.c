ee45f2ba1b4f726cf27676db6a7a27bfd47f8f5f
Stace-cmd <stacykerongo@gmail.com>
2023-04-27 08:05:13 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,13 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
 int n = 97;
 while (n <= 122)
 {
-	putchar (n);
-	n++;
+putchar (n);
+n++;
 }
 putchar('\n');
 return (0);
