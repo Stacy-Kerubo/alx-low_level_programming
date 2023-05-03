a0cda15f032c78faf0ad64c8bf3aec3f37990eb9
Stace-cmd <stacykerongo@gmail.com>
2023-05-03 14:09:32 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,11 +5,12 @@
  * @c: the character to be checked
  *
  * Return: 1 if c is lowercase, 0 otherwise
- */
-int _islower(int c) {
-  if (c >= 'a' && c <= 'z') {
-    return 1;  /* c is lowercase */
-  } else {
-    return 0;  /* c is not lowercase */
+ * /
+int _islower(int c)
+{
+  if (c >= 'a' && c <= 'z')
+{
+    return (1); /* c is lowercase */
   }
+  return (0); /* c is not lowercase */
 }
