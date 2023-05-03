aa89d2ce27a8032f345db50eecdea20c74d130b0
Stace-cmd <stacykerongo@gmail.com>
2023-05-03 03:38:16 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-
-int main(void) {
-    // ASCII code for '0' is 48
-    for (int i = 0; i < 10; i++) {
-        putchar(i + 48);
-    }
-    putchar('\n');
-    return 0;
+/**
+ * main - Entry point
+ * Description: 'Print numbers of base 10 using putchar'
+ * Return: Always 0
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar(n + 48);
+}
+putchar('\n');
+return (0);
 }
 
