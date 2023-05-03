19f7a8bfce950748008dea335afb3b039d81edbd
Stace-cmd <stacykerongo@gmail.com>
2023-05-03 14:01:33 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower - checks whether a given character is lowercase or not
+ *
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c) {
+  if (c >= 'a' && c <= 'z') {
+    return 1;  /* c is lowercase */
+  } else {
+    return 0;  /* c is not lowercase */
+  }
+}
