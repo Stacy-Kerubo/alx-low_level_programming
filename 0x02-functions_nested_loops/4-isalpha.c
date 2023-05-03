a11d074a15e08d3ee80023cb666531643ae7a7a6
Stace-cmd <stacykerongo@gmail.com>
2023-05-03 14:19:41 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - checks whether a given character is an alphabetic character or not.
+ *
+ * @c: the character to be checked.
+ *
+ * Return: 1 if c is an alphabetic character, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1); /* c is an alphabetic character */
+	}
+	return (0); /* c is not an alphabetic character */
+}
+
