74eeee68d3515287d697bf804da9b20c3f72e96b
Stace-cmd <stacykerongo@gmail.com>
2023-05-03 15:23:03 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+int len = 0;
+while (*s++)
+len++;
+return (len);
+}
+
+
