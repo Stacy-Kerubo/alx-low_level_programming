404a05aa9ad07358c5164e7d6293703484520b6f
Stace-cmd <stacykerongo@gmail.com>
2023-05-03 15:02:55 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of the variable pointed to by n to 98
+ * @n: a pointer to an int
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
