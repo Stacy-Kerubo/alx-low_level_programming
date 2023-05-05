7a6556262a961b0627ebab09ec1d710d1376926d
Stace-cmd <stacykerongo@gmail.com>
2023-05-05 05:30:31 -0700

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers
+ *
+ * @a: Pointer to the first element of the array
+ * @n: Number of elements of the array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+int tmp;
+int i;
+for (i = 0; i < n / 2; i++)
+{
+tmp = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = tmp;
+}
+}
+
