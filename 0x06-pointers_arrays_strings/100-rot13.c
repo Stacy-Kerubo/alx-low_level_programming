98837c328c26d89331141f487ed3db8606884c67
Stace-cmd <stacykerongo@gmail.com>
2023-05-05 06:00:16 -0700

    Task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: string to encode
+ *
+ * Return: pointer to encoded string
+ */
+char *rot13(char *s)
+{
+int i, j;
+char *from = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char *to = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; from[j] != '\0'; j++)
+{
+if (s[i] == from[j])
+{
+s[i] = to[j];
+break;
+}
+}
+}
+return (s);
+}
