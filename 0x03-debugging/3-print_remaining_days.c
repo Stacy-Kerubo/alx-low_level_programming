5ce48b3ef7c2d1b4388d400e21ae774aabbc447f
Stace-cmd <stacykerongo@gmail.com>
2023-05-06 14:53:40 -0700

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,44 +1,35 @@
-#include <stdio.h>
-#include "main.h"
-
 /**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
 
 void print_remaining_days(int month, int day, int year)
 {
-    if (month < 1 || month > 12 || day < 1 || day > 31)
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
     {
-        printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-        return;
+        if (month > 2)
+        {
+            day++;
+        }
+
+        printf("Day of the year: %d\n", day);
+        printf("Remaining days: %d\n", 366 - day);
     }
-
-    int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+    else
     {
-        days_in_month[1] = 29;
+        if (month == 2 && day == 60)
+        {
+            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+        }
+        else
+        {
+            printf("Day of the year: %d\n", day);
+            printf("Remaining days: %d\n", 365 - day);
+        }
     }
-
-    if (day > days_in_month[month - 1])
-    {
-        printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-        return;
-    }
-
-    int day_of_year = 0;
-    for (int i = 0; i < month - 1; i++)
-    {
-        day_of_year += days_in_month[i];
-    }
-    day_of_year += day;
-
-    printf("Day of the year: %d\n", day_of_year);
-    printf("Remaining days: %d\n", 365 + ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ? 1 : 0) - day_of_year);
 }
 
