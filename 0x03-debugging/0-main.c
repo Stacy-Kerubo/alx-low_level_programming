f084632f39a3fe307b5b5a869a27f8098baa287a
Stace-cmd <stacykerongo@gmail.com>
2023-05-06 14:14:41 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests the positive_or_negative function with 0 as input
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    int zero = 0;
+
+    positive_or_negative(zero);
+
+    return (0);
+}
