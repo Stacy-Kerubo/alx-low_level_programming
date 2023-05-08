aa20b818e77b76bfbe4ce66125031c9b924add06
Stace-cmd <stacykerongo@gmail.com>
2023-05-08 13:14:03 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,15 +4,13 @@
  * @s: pointer to memory area.
  * @b: constant byte.
  * @n: number of bytes to be filled.
- *
  * Return: pointer to memory area @s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int i;
-
-        for (i = 0; i < n; i++)
-                *(s + i) = b;
-        return (s);
+unsigned int i;
+for (i = 0; i < n; i++)
+*(s + i) = b;
+return (s);
 }
 
