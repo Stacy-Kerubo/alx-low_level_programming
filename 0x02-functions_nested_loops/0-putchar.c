aa841e6a3eb44e418ef53b288b512262e9409fc9
Stace-cmd <stacykerongo@gmail.com>
2023-05-09 04:33:25 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
-int main()
+#include <unistd.h>
+int main(void)
 {
-printf("_putchar\n");
-return 0;
+char c = '_';
+write(1, &c, 1);
+c = 'p';
+write(1, &c, 1);
+c = 'u';
+write(1, &c, 1);
+c = 't';
+write(1, &c, 1);
+c = 'c';
+write(1, &c, 1);
+c = 'h';
+write(1, &c, 1);
+c = 'a';
+write(1, &c, 1);
+c = 'r';
+write(1, &c, 1);
+c = '\n';
+write(1, &c, 1);
+return (0);
 }
+
