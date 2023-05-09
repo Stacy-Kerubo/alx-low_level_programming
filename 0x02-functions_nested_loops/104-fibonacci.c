ad05940dc84871e188453be9c7696032290f2906
Stace-cmd <stacykerongo@gmail.com>
2023-05-09 04:17:21 -0700

    Task 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* Description: prints the first 98 Fibonacci numbers,
-* starting with 1 and 2, followed by a new line.
-* The numbers should be separated by comma, followed by a space.
-* Return: Always 0 (Success)
+* print_fibonacci - Prints the first n Fibonacci numbers
+* starting with 1 and 2, separated by comma and a space.
+* @n: The number of Fibonacci numbers to print.
+* Return: Nothing.
 */
-void print_fibonacci(int n);
+void print_fibonacci(int n)
+{
+int i, n1 = 1, n2 = 2, n3;
+if (n >= 1)
+printf("%d", n1);
+if (n >= 2)
+printf(", %d", n2);
+for (i = 3; i <= n; i++)
+{
+n3 = n1 + n2;
+printf(", %d", n3);
+n1 = n2;
+n2 = n3;
+}
+printf("\n");
+}
+/**
+* main - Calls the print_fibonacci function with n = 98.
+* Return: Always 0.
+*/
 int main(void)
 {
 print_fibonacci(98);
-printf("\n");
-return 0;
-}
-void print_fibonacci(int n)
-{
-int a = 1, b = 2, c;
-printf("%d, %d", a, b);
-for (int i = 3; i <= n; i++) {
-c = a + b;
-printf(", %d", c);
-a = b;
-b = c;
-}
+return (0);
 }
 
