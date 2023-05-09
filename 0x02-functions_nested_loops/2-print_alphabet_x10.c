51e726b5e32dd52c1e658404212a6a77164de7cf
Stace-cmd <stacykerongo@gmail.com>
2023-05-09 02:59:44 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * main - Entry point
-* print_alphabet_x10 - Prints the alphabet 10 times
+* print_alphabet_x10 - prints the lowercase alphabet 10 times, followed by a newline
 * Return: Always 0
 */
 void print_alphabet_x10(void)
