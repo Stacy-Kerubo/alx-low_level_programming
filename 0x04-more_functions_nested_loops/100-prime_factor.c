972e203c75381da26e483b3c0cff4e22741dac21
Stace-cmd <stacykerongo@gmail.com>
2023-05-09 08:51:26 -0700

    Task 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,32 +1,28 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
-
+* main - Entry point
+*
+* Return: Always 0
+*/
 int main(void)
 {
-    long int n = 612852475143;
-    long int i;
-
-    for (i = 2; i <= n; i++)
-    {
-        if (n % i == 0)
-        {
-            while (n % i == 0)
-            {
-                n = n / i;
-            }
-            if (n == 1)
-            {
-                printf("%ld\n", i);
-                break;
-            }
-        }
-    }
-
-    return (0);
+long int n = 612852475143;
+long int i;
+for (i = 2; i <= n; i++)
+{
+if (n % i == 0)
+{
+while (n % i == 0)
+{
+n = n / i;
+}
+if (n == 1)
+{
+printf("%ld\n", i);
+break;
+}
+}
+}
+return (0);
 }
 
