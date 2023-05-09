e09a109f40af31fb32cfd75fb389fb5f08cfaf0c
Stace-cmd <stacykerongo@gmail.com>
2023-05-09 03:54:23 -0700

    Task 103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Prints the sum of even-valued terms
+* in the Fibonacci sequence whose values do not exceed 4,000,000
+* followed by a new line.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int prev = 1, curr = 2, sum = 0, temp;
+while (curr <= 4000000)
+{
+if (curr % 2 == 0)
+sum += curr;
+temp = curr;
+curr += prev;
+prev = temp;
+}
+printf("%d\n", sum);
+return 0;
+}
+
