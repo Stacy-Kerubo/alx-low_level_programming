79b811832a00d572c0a66c8be9fdc7166fb984d6
Stace-cmd <stacykerongo@gmail.com>
2023-05-09 09:04:53 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,22 @@
 #include "main.h"
-
 /**
- * print_line - Prints a straight line in the terminal
- * @n: The number of times to print the '_'
- */
-
+* print_line - Prints a straight line in the terminal
+* @n: The number of times to print the '_'
+*/
 void print_line(int n)
 {
-    int i;
-
-    if (n > 0)
-    {
-        for (i = 0; i < n; i++)
-        {
-            _putchar('_');
-        }
-        _putchar('\n');
-    }
-    else
-    {
-        _putchar('\n');
-    }
+int i;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
 }
 
