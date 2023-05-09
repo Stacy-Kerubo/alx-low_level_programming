dd80391a7091e149d21f05159b7c8a7b8b10f1f2
Stace-cmd <stacykerongo@gmail.com>
2023-05-09 03:10:51 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* times_table - prints the 9 times table, starting with 0.
+* Return: void
+*/
+void times_table(void)
+{
+int i, j, result;
+for (i = 0; i < 10; i++)
+{
+_putchar('0');
+for (j = 1; j < 10; j++)
+{
+_putchar(',');
+_putchar(' ');
+result = i * j;
+if (result < 10)
+{
+_putchar(' ');
+_putchar(result + '0');
+}
+else
+{
+_putchar(result / 10 + '0');
+_putchar(result % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
