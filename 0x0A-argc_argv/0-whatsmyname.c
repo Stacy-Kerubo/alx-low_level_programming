e00530676177244caa284430ad1716f380401097
Stace-cmd <stacykerongo@gmail.com>
2023-05-13 23:37:29 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - Entry point. Prints the name of the program.
+* @argc: Number of command-line arguments.
+* @argv: Array of pointers to strings representing the arguments.
+* Return: Always 0.
+*/
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return 0;
+}
