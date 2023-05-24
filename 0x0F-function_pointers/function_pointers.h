b31501d6003eb49cbcc357a0befc63b3258050f2
Stace-cmd <stacykerongo@gmail.com>
2023-05-24 14:30:18 -0700

    Task 0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/* Function prototype for print_name */
+void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H */
+
